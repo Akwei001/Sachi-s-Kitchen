d5eea0e77d3277abaf251de368e9e042d28be89e
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2023-11-28 18:04:47 +0000

    card working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,51 @@
+import {  Card, 
+  CardContent, 
+  CardDescription, 
+  CardFooter, 
+  CardHeader, 
+  CardTitle  } from "@/components/ui/card"
+
 
 
 
 export default function Home() {
   return (
     <main className="">
+      <nav>
+                                                                                                                    
+        </nav>
       <h1 className="text-lg">Sachis Kitchen</h1>
+      <div>
+        <Card>
+          <CardHeader>
+            <CardTitle>Card Title</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+          </CardHeader>
+          <CardContent>Card Content</CardContent>
+          <CardFooter>Card Footer</CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <CardTitle>Card Title</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+          </CardHeader>
+          <CardContent>Card Content</CardContent>
+          <CardFooter>Card Footer</CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <CardTitle>Card Title</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+          </CardHeader>
+          <CardContent>Card Content</CardContent>
+          <CardFooter>Card Footer</CardFooter>
+        </Card>
+     
+          <CardHeader>
+            <CardTitle>Card Title</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+          </CardHeader>
+      </div>
     </main>
   )
 }
