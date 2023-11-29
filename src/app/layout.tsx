f63833915c7e61b0724835c42efb99d3a69693d8
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2023-11-29 21:58:50 +0000

    navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { NavigationMenu } from '@/components/ui/navigation-menu'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,7 +19,7 @@ export default function RootLayout({
     <html lang="en">
      
       <body className={inter.className}>{children}</body>
-  
+    <footer>Test Footer</footer>
     </html>
   )
 }
