1e5f60ee0015e6bd677f3f575de0e41826527853
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2023-12-17 09:42:38 +0000

    feature section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ import {  Card,
 
   import { Button } from "@/components/ui/button"
   import Slider from "@/components/ui/Slider"
+  import Image from "next/image"
 
 
 
@@ -21,10 +22,30 @@ export default function Home() {
       {/*Showcase mouth-watering images of izakaya-inspired dishes with a welcome message. */}
       <Slider/>
       {/*Highlight the unique experience of Japanese street food at the pop-up restaurant.*/}
-      <section>Feature Section
-        <h1>Experience the Flavours of Japan with Us</h1>
-        <h1></h1>
-        <h1></h1>
+      <section>
+        
+        <div>
+          <h2>Delicious</h2>
+          <h1>Experience the Authentic Flavours of Japanese Street Food</h1>
+        <p>At our pop-up reatauranr, we offer a unique dining experience where you can indluge in the vibrant flavour of the world a Japanese Street food. Our menu is inspired by izakaya-style cuisine, featuring a variety of small plates and snacks that are perfevt for sharing. Whether you are a fan of Tempura or yakitori, our talented chefs prepare each dish with care and precision, ensuring bite is a burst of authentic Japanese flavours. Join us and embark on a culinary journey through the bustling streets of Japan. </p>
+        <div>
+          <h3>Taste Japan</h3>
+          <p>Savour the bold and unique flavours of Japanese Street food.</p>
+        </div>
+        <div>
+          <h3>Discover More</h3>
+          <p>Explore our menu and experience the vibrant culinary cuture of Japan.</p>
+        </div>
+        <button>Menu</button>
+        <button>Reserve  + </button>
+      </div>
+      <div>
+        <Image src='/a_photo_of_gyoza.png'
+        alt='a photo of gyoza'
+        width={500}
+        height={500}/>
+      </div>
+       
       </section>
       {/*Invite visitors to explore the menu and place an order for delivery or book a table.*/}
       <section>CTA Section</section>
