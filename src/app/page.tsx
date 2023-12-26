b6b782a922f07bde83c33c7aedffc2ebf51462f3
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2023-12-26 10:35:21 +0000

    outline for layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,27 +22,27 @@ export default function Home() {
       {/*Showcase mouth-watering images of izakaya-inspired dishes with a welcome message. */}
       <Slider/>
       {/*Highlight the unique experience of Japanese street food at the pop-up restaurant.*/}
-      <section className=" grid grid-cols-2 m-auto">
+      <section className="grid grid-cols-2 m-auto ">
         
-        <div className="">
-          <h2>Delicious</h2>
-          <h1>Experience the Authentic Flavours of Japanese Street Food</h1>
-        <p>At our pop-up reatauranr, we offer a unique dining experience where you can indluge in the vibrant flavour of the world a Japanese Street food. Our menu is inspired by izakaya-style cuisine, featuring a variety of small plates and snacks that are perfevt for sharing. Whether you are a fan of Tempura or yakitori, our talented chefs prepare each dish with care and precision, ensuring bite is a burst of authentic Japanese flavours. Join us and embark on a culinary journey through the bustling streets of Japan. </p>
-        <div>
-          <h3>Taste Japan</h3>
-          <p>Savour the bold and unique flavours of Japanese Street food.</p>
+        <div className="border border-red-500">
+          <h2 className="border border-red-500 ">Delicious</h2>
+          <h1 className="border border-red-500 ">Experience the Authentic Flavours of Japanese Street Food</h1>
+        <p className="border border-red-500 ">At our pop-up reatauranr, we offer a unique dining experience where you can indluge in the vibrant flavour of the world a Japanese Street food. Our menu is inspired by izakaya-style cuisine, featuring a variety of small plates and snacks that are perfevt for sharing. Whether you are a fan of Tempura or yakitori, our talented chefs prepare each dish with care and precision, ensuring bite is a burst of authentic Japanese flavours. Join us and embark on a culinary journey through the bustling streets of Japan. </p>
+        <div className="border border-red-500 ">
+          <h3 className="border border-red-500 ">Taste Japan</h3>
+          <p className="border border-red-500 ">Savour the bold and unique flavours of Japanese Street food.</p>
         </div>
-        <div>
-          <h3>Discover More</h3>
+        <div className="border border-red-500 ">
+          <h3 className="border border-red-500 ">Discover More</h3>
           <p>Explore our menu and experience the vibrant culinary cuture of Japan.</p>
         </div>
-        <Button>
+        <Button className="border border-red-500 ">
           <button>Menu</button>
           </Button>
-        <Button>Reserve  + </Button>
+        <Button className="border border-red-500 ">Reserve  + </Button>
         
       </div>
-      <div>
+      <div className="border border-red-500 ">
         <Image src='/a_photo_of_gyoza.png'
         alt='a photo of gyoza'
         width={500}
