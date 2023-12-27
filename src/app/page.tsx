29706d491c151695dc45bd01c68cdd5e841e9789
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2023-12-27 21:31:07 +0000

    main layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,31 +18,36 @@ import {  Card,
 export default function Home() {
   return (
     <main className="">
-      <h1 className="text-lg">Sachis Kitchen</h1>
+      <h1 className="text-lg ">Sachis Kitchen</h1>
       {/*Showcase mouth-watering images of izakaya-inspired dishes with a welcome message. */}
       <Slider/>
       {/*Highlight the unique experience of Japanese street food at the pop-up restaurant.*/}
-      <section className="grid grid-cols-2 m-auto ">
+      <section className="grid grid-cols-2 m-4 ">
         
         <div className="border border-red-500">
-          <h2 className="border border-red-500 ">Delicious</h2>
-          <h1 className="border border-red-500 ">Experience the Authentic Flavours of Japanese Street Food</h1>
-        <p className="border border-red-500 ">At our pop-up reatauranr, we offer a unique dining experience where you can indluge in the vibrant flavour of the world a Japanese Street food. Our menu is inspired by izakaya-style cuisine, featuring a variety of small plates and snacks that are perfevt for sharing. Whether you are a fan of Tempura or yakitori, our talented chefs prepare each dish with care and precision, ensuring bite is a burst of authentic Japanese flavours. Join us and embark on a culinary journey through the bustling streets of Japan. </p>
-        <div className="border border-red-500 ">
-          <h3 className="border border-red-500 ">Taste Japan</h3>
+          <h2 className="mb-2 border border-red-500">Delicious</h2>
+          <h1 className="mb-4 text-lg font-bold border border-red-500">Experience the Authentic Flavours of Japanese Street Food</h1>
+        <p className="mb-10 border border-red-500 ">At our pop-up restaurant, we offer a unique dining experience where you can indluge in the vibrant flavour of the world a Japanese Street food. Our menu is inspired by izakaya-style cuisine, featuring a variety of small plates and snacks that are perfevt for sharing. Whether you are a fan of Tempura or yakitori, our talented chefs prepare each dish with care and precision, ensuring bite is a burst of authentic Japanese flavours. Join us and embark on a culinary journey through the bustling streets of Japan. </p>
+        <div className="flex mb-6 border border-green-500">
+          <div className="border border-red-500 ">
+          <h3 className="font-semibold border border-red-500">Taste Japan</h3>
           <p className="border border-red-500 ">Savour the bold and unique flavours of Japanese Street food.</p>
         </div>
         <div className="border border-red-500 ">
-          <h3 className="border border-red-500 ">Discover More</h3>
+          <h3 className="font-semibold border border-red-500 ">Discover More</h3>
           <p>Explore our menu and experience the vibrant culinary cuture of Japan.</p>
         </div>
-        <Button className="border border-red-500 ">
-          <button>Menu</button>
+        </div>
+        
+        <Button className="mr-2 border border-red-500 ">
+          Menu
           </Button>
-        <Button className="border border-red-500 ">Reserve  + </Button>
+        <Button className="border border-red-500 ">
+          Reserve  + 
+          </Button>
         
       </div>
-      <div className="border border-red-500 ">
+      <div className="flex justify-center border border-red-500 ">
         <Image src='/a_photo_of_gyoza.png'
         alt='a photo of gyoza'
         width={500}
