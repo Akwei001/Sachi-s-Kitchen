7b9944e78efeeb915e8c181e35bb1b6a5ea10bff
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2023-12-29 12:24:57 +0000

    category page layout

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { menu } from '@/data'
+import Link from 'next/link'
 
 const MenuPage = () => {
   return (
-    <div>MenuPage</div>
+    <div className='p-4 lg:px-20 xl:px-40 h-[100vh] md:h-[100vh]flex flex-col md:flex-row items-center '>
+      {menu.map(category=>(
+      <Link 
+      href={category.slug} 
+      key={category.id}
+      className='border border-red-500'
+      // style={{backgroundImage: `url(${category.color})`}}
+      >
+      <div className={`text-${category.color} w-12`}>
+        <h1 className='text-3xl font-bold uppercase'>{category.title}</h1>
+        <p className='my-8 text-sm'>{}</p>
+        <button className={`hidden 2xl:block`}>Explore</button>
+      </div>
+      </Link>
+      ))}
+    </div>
   )
 }
 
--- src/data.ts
+++ src/data.ts
@@ -8,3 +8,58 @@ type Product = {
   };
   
   type Products = Product[];
+
+  type Menu = {
+    id: number;
+    slug: string;
+    title: string;
+    desc?: string;
+    img?: string;
+    color: string;
+  }[];
+
+  export const menu: Menu = [
+   {
+    id: 1,
+    slug: 'Onigiri',
+    title: 'Onigiri',
+    desc: 'A traditional Japanese rice dish that is made with rice and fish. It is typically served with a variety of condiments, such as soy sauce, wasabi, and ginger.',
+    img: '/onigiri.png',
+    color: 'red',
+   },  
+   {
+    id: 2,
+    slug: 'Sides',
+    title: 'Sides',
+    desc: 'A range of small dishes including Gyoza, Yakitori and Karage',
+    img: '/a_photo_of_gyoza.png',
+    color: 'blue',
+   }, 
+   {
+    id: 3,
+    slug: 'Ramen',
+    title: 'Ramen',
+    desc: 'A selection of Ramen dishes',
+    img: '/yakitori.png',
+    color: 'green',
+   },
+   {
+    id: 4,
+    slug: 'Rice',
+    title: 'Rice',
+    desc: 'A selection of Rice dishes',
+    img: '/yakitori.png',
+    color: 'green',
+   },
+  //  {
+  //   id: ,
+  //   slug: '',
+  //   title: '',
+  //   desc: '',
+  //   img: '',
+  //   color: ''
+  //  },       
+   
+   
+
+  ];
