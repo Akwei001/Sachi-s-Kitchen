968de9459853dbd9a44b837d30f9287005b11829
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2023-12-30 21:50:34 +0000

    card component added to menu

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { menu } from '@/data'
 import Link from 'next/link'
+import { Card } from '@/components/ui/card'
 
 const MenuPage = () => {
   return (
     <div className='p-4 lg:px-20 xl:px-40 h-[100vh] md:h-[100vh]flex flex-col md:flex-row items-center '>
       {menu.map(category=>(
-      <Link 
+        <> 
+        <Card>
+          <Link 
       href={category.slug} 
       key={category.id}
       className='border border-red-500'
@@ -14,10 +17,14 @@ const MenuPage = () => {
       >
       <div className={`text-${category.color} w-12`}>
         <h1 className='text-3xl font-bold uppercase'>{category.title}</h1>
-        <p className='my-8 text-sm'>{}</p>
+        <p className=''>{category.desc}</p>
         <button className={`hidden 2xl:block`}>Explore</button>
       </div>
       </Link>
+      </Card>
+      </>
+       
+      
       ))}
     </div>
   )
