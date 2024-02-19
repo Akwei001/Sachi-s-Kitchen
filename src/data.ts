b67589e18a942988d99e824027d8a37a0bafab51
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2024-02-19 21:53:12 +0000

    new data

--- src/data.ts
+++ src/data.ts
@@ -130,6 +130,27 @@ type Product = {
         },
       ],
     },
+    {
+      id: 3,
+      title: "Katsudon",
+      desc: "",
+      img: "/temporary/p1.png",
+      price: 5,
+      options: [
+        {
+          title: "Small",
+          additionalPrice: 0,
+        },
+        {
+          title: "Medium",
+          additionalPrice: 4,
+        },
+        {
+          title: "Large",
+          additionalPrice: 6,
+        },
+      ],
+    },
 
 
 
